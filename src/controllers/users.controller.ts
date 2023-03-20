c70a83429781693cfe97beb2fabce3b1e389da03
Camilo-neck <ccuello@unal.edu.co>
2023-03-20 15:03:12 -0500

    Updating user from table

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,3 +2,18 @@ export const getUsers = () => {
 	const users = JSON.parse(localStorage.getItem('users') || '');
 	return users ? users : [];
 }
+
+export const addUser = (user: any) => {
+	const users = getUsers();
+	localStorage.setItem('users', JSON.stringify([...users, user]));
+}
+
+export const deleteUser = (id: number) => {
+	const users = getUsers();
+	localStorage.setItem('users', JSON.stringify(users.filter((user: any) => user.id !== id)));
+}
+
+export const updateUser = (user: any) => {
+	const users = getUsers();
+	localStorage.setItem('users', JSON.stringify(users.map((u: any) => u.id === user.id ? user : u)));
+}
--- src/manage/pages/students.tsx
+++ src/manage/pages/students.tsx
@@ -1,5 +1,5 @@
 import Layout from '@/components/layout';
-import { getUsers } from '@/controllers/users.controller';
+import { getUsers, updateUser } from '@/controllers/users.controller';
 import { shuffleArray } from '@/utils/others.utils';
 import React, { useEffect } from 'react';
 
@@ -18,17 +18,17 @@ const Students = () => {
 	const [ rows, setRows ] = React.useState<any[]>([]);
 
 	const columns: GridColDef[] = [
-		{ field: 'id', headerName: 'ID', width: 70, editable: true },
-		{ field: 'first_name', headerName: 'Primer Nombre', width: 130, editable: true },
-		{ field: 'second_name', headerName: 'Segundo nombre', width: 130, editable: true },
-		{ field: 'surname', headerName: 'Primer apellido', width: 130, editable: true },
-		{ field: 'second_surname', headerName: 'Segundo apellido', width: 130, editable: true },
-		{ field: 'email', headerName: 'Correo', width: 130, editable: true },
-		{ field: 'rol_id', headerName: 'Rol', width: 130, editable: true },
-		{ field: 'type_document', headerName: 'Tipo de documento', width: 130, editable: true },
-		{ field: 'document_number', headerName: 'Documento', width: 130, editable: true },
-		{ field: 'phone', headerName: 'Teléfono', width: 130, editable: true },
-		{ field: 'state', headerName: 'Estado', width: 130, valueGetter: (params) => params.row.state ? 'Activo' : 'Inactivo' },
+		{ field: 'id', headerName: 'ID', headerClassName: "bg-[#FFDDAF]", width: 70, editable: true },
+		{ field: 'first_name', headerName: 'Primer Nombre', headerClassName: "bg-[#FFDDAF]", width: 130, editable: true },
+		{ field: 'second_name', headerName: 'Segundo nombre', headerClassName: "bg-[#FFDDAF]", width: 130, editable: true },
+		{ field: 'surname', headerName: 'Primer apellido', headerClassName: "bg-[#FFDDAF]", width: 130, editable: true },
+		{ field: 'second_surname', headerName: 'Segundo apellido', headerClassName: "bg-[#FFDDAF]", width: 130, editable: true },
+		{ field: 'email', headerName: 'Correo', headerClassName: "bg-[#FFDDAF]", width: 130, editable: true },
+		{ field: 'rol_id', headerName: 'Rol', headerClassName: "bg-[#FFDDAF]", width: 130, editable: true },
+		{ field: 'type_document', headerName: 'Tipo de documento', headerClassName: "bg-[#FFDDAF]", width: 130, editable: true },
+		{ field: 'document_number', headerName: 'Documento', headerClassName: "bg-[#FFDDAF]", width: 130, editable: true },
+		{ field: 'phone', headerName: 'Teléfono', headerClassName: "bg-[#FFDDAF]", width: 130, editable: true },
+		{ field: 'state', headerName: 'Estado', headerClassName: "bg-[#FFDDAF]", width: 130, valueGetter: (params) => params.row.state ? 'Activo' : 'Inactivo' },
 	]
 
 	useEffect(() => {
@@ -42,39 +42,49 @@ const Students = () => {
 		setRows(students)
 	}, [students]);
 
+	const processRowUpdate = React.useCallback(
+		(newRow) => {
+			setStudents((prev: any) => {
+				const index = prev.findIndex((user: any) => user.id === newRow.id);
+				// prev is readonly, so we need to create a new array
+				const newUsers = [...prev];
+				newUsers[index] = { ...newUsers[index], ...newRow };
+				return newUsers;
+			});
+			updateUser(newRow);
+		},
+		[setStudents],
+	);
+
 	return (
 		<Layout>
 			<>
 				<p className='text-4xl font-bold text-[#3D2E16] mt-10 text-center'>Gestión de Estudiantes</p>
-				<div className='flex flex-col items-center h-[70vh] w-full mt-10'>
-					<DataGrid
-						columns={columns}
-						rows={rows}
-						initialState={{
-							pagination: {
-								paginationModel: {
-									pageSize: 10,
+				<div className='flex flex-col items-center h-[70vh] w-screen mt-10'>
+					<div className="self-center w-[90vw] h-full">
+						<DataGrid
+							columns={columns}
+							rows={rows}
+							editMode='row'
+							initialState={{
+								pagination: {
+									paginationModel: {
+										pageSize: 10,
+									},
 								},
-							},
-						}}
-						pageSizeOptions={[5, 10, 20]}
-						onCellEditStop={(e: GridCellEditStopParams, params: MuiEvent) => {
-							// e is the event
-							// e.row is the user
-							// Update the users list
-							setStudents((prev: any) => {
-								const index = prev.findIndex((user: any) => user.id === e.row.id);
-								// prev is readonly, so we need to create a new array
-								const newUsers = [...prev];
-								newUsers[index] = e.row;
-								return newUsers;
-							});
-						}}
-						checkboxSelection
-						disableRowSelectionOnClick
-					/>
+							}}
+							pageSizeOptions={[5, 10, 20]}
+							processRowUpdate={processRowUpdate}
+							onProcessRowUpdateError={(error) => console.log(error)}
+							onCellEditStop={(params: GridCellEditStopParams) => {
+								console.log(params);
+							}}
+							checkboxSelection
+							disableRowSelectionOnClick
+						/>
+					</div>
 				</div>
-				<Button onClick={() => console.log(rows)} variant='contained' color='primary' className='mt-10'>Guardar</Button>
+				{/*<Button onClick={() => console.log(rows)} variant='contained' color='primary' className='mt-10'>Guardar</Button>*/}
 			</>
 		</Layout>
 	);
